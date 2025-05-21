0ef1dc5683ce7981220fff6c9f3f2d72ecaa1619
Miguel Angel Feria Padilla <migferiapad@gmail.com>
2025-05-21 13:03:35 -0600

    Header colors

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 
 function Header() {
+  const [active, setActive] = useState('inicio');
+
   return (
     <nav className="header-content">
       <ul>
-        <li><a href="#inicio">Inicio</a></li>
-        <li><a href="#acerca">Acerca</a></li>
-        <li><a href="#contacto">Contacto</a></li>
+        <li>
+          <a
+            href="#inicio"
+            className={active === 'inicio' ? 'active main-menu' : ''}
+            onClick={() => setActive('inicio')}
+          >
+            Main Menu
+          </a>
+        </li>
+        <li>
+          <a
+            href="#acerca"
+            className={active === 'acerca' ? 'active characters' : ''}
+            onClick={() => setActive('acerca')}
+          >
+            Characters
+          </a>
+        </li>
+        <li>
+          <a
+            href="#contacto"
+            className={active === 'contacto' ? 'active about' : ''}
+            onClick={() => setActive('contacto')}
+          >
+            About
+          </a>
+        </li>
       </ul>
     </nav>
   );
